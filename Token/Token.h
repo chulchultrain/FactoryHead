4faa60da9c29622a5f6b610f0e5db59856ec8324
Thomas Yang <yangchulmin0@gmail.com>
2016-07-21 18:40:28 -0500

    Added Token

--- Token/Token.h
+++ Token/Token.h
@@ -7,15 +7,40 @@ using namespace std;
 
 /*
 	These Tokens will be used for holding info about the Battle Factory
-	pokemon.
+	pokemon. Main purpose is to be able to use the TokenType to call the
+	correct Token Evaluator.
 */
 
 class Token {
 	public:
+		/*
+			the Type of Token.
+		*/
 		enum TokenType { NONE,TYPE, NAME, MOVE, ROUND };
+		/*
+			@return a Token that has a NONE Type and an empty string value.
+		*/
 		Token();
-		Token(TokenType t, string s);
+		/*
+			@param t - the type of Token to make.
+			Valid types t are string forms of the enum values to create
+			their corresponding enum values.
+			Precondition: Valid t
+
+			@param s - the value of the Token to make. 
+			@return - a token with a type t and a string val of s.
+
+			string val of s
+		*/
+		Token(string t, string s);
+		/*
+			@return - the string value of the Token.
+		*/
 		string getVal();
+		/*
+			@return - the enum value of the Token representing the 
+				type of Token it is.
+		*/
 		TokenType getTokenType();
 	private:
 		TokenType tt;
--- Token/TokenTester.cpp
+++ Token/TokenTester.cpp
@@ -2,19 +2,50 @@
 #include <iostream>
 #include <stdlib.h>
 #include <map>
+#include <cassert>
+#include <vector>
 
 using namespace std;
 
 /*
-	
+	Token Test Function. Will test the constructor and whether the proper values
+	can be retrieved from the class instance. 
+
+	Types(as specified by the enum in the header file):
+	NONE,TYPE, NAME, MOVE, ROUND
+
+	Can have values of empty and non-empty strings.
+	Non-empty have a space
 */
+void TokenUnitTest() {
+
+	Token a("TYPE","FIRE");
+	
+	assert(a.getTokenType() == Token::TYPE && a.getVal() == "FIRE" && "1");
+
+	Token b("NONE","WATER");
+
+	assert(b.getTokenType() == Token::NONE && b.getVal() == "WATER" && "2");
+	
+	Token c("NAME","MOVE");
+
+	assert(c.getTokenType() == Token::NAME && c.getVal() == "MOVE" && "3");
+
+	Token d("MOVE","Flamethrower");
+
+	assert(d.getTokenType() == Token::MOVE && d.getVal() == "Flamethrower" && "4");
+
+	Token e("ROUND","Solar Beam");
+
+	assert(e.getTokenType() == Token::ROUND && e.getVal() == "Solar Beam" && "5");
+
+
+
+}
+
 
 int main() {
-	Token *t = new Token(Token::TYPE,"herpes");
-	if(t->getTokenType() == Token::TYPE)
-		cout << "SUCCESS\n";
-	else
-		cout << "FAILURE\n";
+	TokenUnitTest();
 	return 0;
 
 
