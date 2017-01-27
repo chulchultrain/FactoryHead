581d5c2cb4498a2cb7b03dcba85ad82a5bbbb196
Thomas Yang <yangchulmin0@gmail.com>
2017-01-27 11:01:04 -0600

    fix new bug js

--- js/loadData.js
+++ js/loadData.js
@@ -2,7 +2,7 @@
 
 
 function loadDoc(url,doFunc) {
-	var xhttp = XMLHttpRequest();
+	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if(this.readyState == 4 && this.status == 200) {
 			doFunc(xhttp);
